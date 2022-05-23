d940f4f62ff464d1b64cbc1c6c02754f708a1cb7
formateur1 <enseignant.formateur@protonmail.com>
2022-05-23 12:44:23 +0200

    Ajout de la fonction findRolesByUsername Création de UserDetailsServiceImpl

--- src/main/java/com/inti/SpringBootValidationThymeleaf/repository/RoleRepository.java
+++ src/main/java/com/inti/SpringBootValidationThymeleaf/repository/RoleRepository.java
@@ -2,10 +2,19 @@ package com.inti.SpringBootValidationThymeleaf.repository;
 
 import com.inti.SpringBootValidationThymeleaf.model.Role;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+	
+	@Query(value = "select r.* from utilisateur u, utilisateur_role ur, role r\r\n"
+			+ "where u.id = ur.id_utilisateur and ur.id_role = r.id\r\n"
+			+ "and (u.username=:username or u.email=:username);", nativeQuery = true)
+	List<Role> findRolesByUsername(@Param("username") String username);
 
 }
