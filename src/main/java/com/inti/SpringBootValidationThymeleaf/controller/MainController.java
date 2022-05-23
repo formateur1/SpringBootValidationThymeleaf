d46bc5211678c5f52f2e9fa377078aeea3b3715e
formateur1 <enseignant.formateur@protonmail.com>
2022-05-23 10:12:03 +0200

    Mon premier commit comprenant une classe controller qui appelle une page avec Thymeleaf

---
+++ src/main/java/com/inti/SpringBootValidationThymeleaf/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.inti.SpringBootValidationThymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController
+{
+	
+	@GetMapping("/hello")
+	public String hello()
+	{
+		
+		return "hello";
+	}
+
+}
---
+++ src/test/java/com/inti/SpringBootValidationThymeleaf/SpringBootValidationThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inti.SpringBootValidationThymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootValidationThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
